c397d65dd54e9214e04d7629622ab762ef7b4e85
kasi <danielkaser@mailbox.org>
2023-11-08 15:53:00 +0100

    solve 01-excercise-object-constructors.js

---
+++ fullstack-javascript/02-javascript/organizing-your-js-code/01-excercise-object-constructors.js
@@ -0,0 +1,16 @@
+function Book(title, author, numPages, isRead) {
+  this.title = title;
+  this.author = author;
+  this.numPages = numPages;
+  this.isRead = isRead;
+
+  this.info = function () {
+    return `${title} by ${author}. ${numPages} pages, ${
+      isRead ? "already read" : "not read yet."
+    }`;
+  };
+}
+
+const theHobbit = new Book("The Hobbit", "J.R.R. Tokien", 295, false);
+
+console.log(theHobbit.info());
