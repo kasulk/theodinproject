3e6dae11d6732ed741695cb4692e25fa65035127
kasi <danielkaser@mailbox.org>
2023-11-08 17:20:38 +0100

    add to 01-object-and-constructors

--- fullstack-javascript/02-javascript/src/02-organizing-your-js-code/01-object-and-constructors-ex01.js
+++ fullstack-javascript/02-javascript/src/02-organizing-your-js-code/01-object-and-constructors-ex01.js
@@ -17,3 +17,17 @@ console.log(theHobbit.info());
 
 console.log(Object.getPrototypeOf(theHobbit));
 console.log(Book.prototype);
+
+// add function to Book.prototype
+Book.prototype.sayHello = function () {
+  console.log("hello, I'm a book!");
+};
+console.log(Book.prototype);
+theHobbit.sayHello();
+
+// valueOf() is inherited from Object.prototype.valueOf
+console.log(theHobbit.valueOf());
+
+console.log(theHobbit.hasOwnProperty("valueOf"));
+console.log(Object.prototype.hasOwnProperty("valueOf"));
+console.log(Object.prototype.hasOwnProperty("hasOwnProperty"));
--- fullstack-javascript/02-javascript/src/02-organizing-your-js-code/01-object-and-constructors-notes.md
+++ fullstack-javascript/02-javascript/src/02-organizing-your-js-code/01-object-and-constructors-notes.md
@@ -5,3 +5,8 @@
 #### 2. Stated simply, the prototype is another object...
 
 #### 3. ...that the original object **_inherits_** from, and has access to all of its prototypes's methods and properties
+
+## Note:
+
+1. Every `prototype` object inherits from `Object.prototype` by default.
+2. An object's `Object.getPrototypeOf()` value can only be **_one_** unique `prototype` object.
